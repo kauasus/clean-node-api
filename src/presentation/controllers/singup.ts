44e9d13d46b883dd5df377dd2da8e7f6ea88acc4
Kaua Felipe Alves Guimaraes <kaua.f.guimaraes@gmail.com>
2026-08-20 07:02:56 -0300

    feat: ensure SingUpController returns 400 if an invalid email is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -3,13 +3,23 @@ import { MissingParamError } from '../erros/missing-parm-error'
 import { badRequest } from '../helpers/helper'
 import { ok } from '../helpers/helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../erros/invalid-parm-error'
 
 export class SingUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
+      }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
       }
     }
 
