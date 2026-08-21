17588e806a7f7abbdc8fd37b5959f94aaff69712
Kaua Felipe Alves Guimaraes <kaua.f.guimaraes@gmail.com>
2026-08-21 12:53:34 -0300

    test: ensure SingUpController returns 400 if password confirmation fails

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -85,6 +85,23 @@ describe('SingUp Controller', () => {
       new MissingParamError('passwordConfirmation')
     )
   })
+  test('Should return 400 if no password confirmation fails', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@example.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password',
+      },
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError('passwordConfirmation')
+    )
+  })
   test('Should return 400 if no an invalid email is provided', () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
