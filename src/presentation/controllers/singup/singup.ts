ddb0a7b665de0f2362b98d0907aa1825919e44ee
Kaua Felipe Alves Guimaraes <kaua.f.guimaraes@gmail.com>
2026-08-21 18:09:45 -0300

    refactor: move success http response to a helper method

--- src/presentation/controllers/singup/singup.ts
+++ src/presentation/controllers/singup/singup.ts
@@ -6,8 +6,7 @@ import {
   AddAccount,
 } from './singup-protocols'
 import { MissingParamError, InvalidParamError } from '../../erros'
-import { badRequest, serverError } from '../../helpers/helper'
-import { ok } from '../../helpers/helper'
+import { badRequest, serverError, ok } from '../../helpers/helper'
 
 export class SingUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -42,13 +41,8 @@ export class SingUpController implements Controller {
         email,
         password,
       })
-      
-      return {
-        
-        statusCode: 200,
-        body: account
-      }
 
+      return ok(account)
     } catch (error) {
       return serverError()
     }
